c8ff70bcb340e9cffd8ebb8c34ad1f886b63878a
Paul Verschoor (on server) <paul.verschoor@gmail.com>
2014-05-25 19:03:16 +0200

    changed on server

--- ip_server.js
+++ ip_server.js
@@ -5,6 +5,7 @@ app.set('title', 'ip-server');
 
 app.use('/',function(req,res){
  	req.setEncoding('utf8');
+ 	app.set('trust proxy', true);
 	res.set('Content-Type', 'text/plain');
 	res.end(req.ip);
 })
